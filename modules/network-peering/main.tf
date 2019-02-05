b80030313d4667659b895beb3459f93126abcad9
Riley Karson <rileykarson@google.com>
2019-02-04 17:04:24 -0800

    Add helper module configs

---
+++ modules/network-peering/main.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network_peering" "first" {
+  name = "${var.name}-first"
+  network = "${var.first_network}"
+  peer_network = "${var.second_network}"
+}
+
+resource "google_compute_network_peering" "second" {
+  name = "${var.name}-second"
+  network = "${var.second_network}"
+  peer_network = "${var.first_network}"
+}
---
+++ modules/project-host-configuration/variables.tf
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These variables are expected to be passed in by the operator
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "project" {
+  description = "The project ID for the project to enable as a host project"
+}
