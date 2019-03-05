d34f2f3e73edcb7de11a057ef1e7e5161313baac
Riley Karson <rileykarson@google.com>
2019-03-05 13:47:23 -0800

    Add a timeout to SSH tests

--- test/network_helpers.go
+++ test/network_helpers.go
@@ -17,6 +17,7 @@ var (
 	// we don't want to retry for too long, but we should do it at least a few times to make sure the instance is up
 	SSHMaxRetriesExpectError = 3
 	SSHSleepBetweenRetries = 3 * time.Second
+	SSHTimeout = 15 * time.Second
 	SSHEchoText = "Hello World"
 )
 
