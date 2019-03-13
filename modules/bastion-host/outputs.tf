c18f25ae9ac1b0823e408b60dc501ab7291bf98a
Riley Karson <rileykarson@google.com>
2019-03-13 15:20:29 -0700

    terraform fmt

--- modules/bastion-host/outputs.tf
+++ modules/bastion-host/outputs.tf
@@ -3,7 +3,6 @@ output "instance" {
   value       = "${google_compute_network.vpc.self_link}"
 }
 
-
 output "address" {
   description = "The public IP of the bastion host."
   value       = "${google_compute_network.vpc.self_link}"
