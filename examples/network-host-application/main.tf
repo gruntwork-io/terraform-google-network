f94fcf72ebf02ee31c200b862b72e24924c43b3d
Riley Karson <rileykarson@google.com>
2019-06-04 11:14:21 -0700

    Bump refs, use https

--- examples/network-host-application/main.tf
+++ examples/network-host-application/main.tf
@@ -1,7 +1,7 @@
 module "application_network" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "git::git@github.com:gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=v0.1.0"
+  # source = "github.com/gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=v0.1.2"
   source = "../../modules/vpc-network"
 
   name_prefix = "${var.name_prefix}"
@@ -12,7 +12,7 @@ module "application_network" {
 module "project_host_configuration" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "git::git@github.com:gruntwork-io/terraform-google-network.git//modules/project-host-configuration?ref=v0.0.1"
+  # source = "github.com/gruntwork-io/terraform-google-network.git//modules/project-host-configuration?ref=v0.1.2"
   source = "../../modules/project-host-configuration"
 
   project = "${var.project}"
