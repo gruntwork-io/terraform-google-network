63fc5a7a6adae47917b04ca97f4551dde2a1af10
Petri Autero <petri@gruntwork.io>
2019-06-05 20:13:37 +0300

    TF12upgrade bastion example

--- examples/bastion-host/outputs.tf
+++ examples/bastion-host/outputs.tf
@@ -1,9 +1,10 @@
 output "address" {
   description = "The public IP of the bastion host."
-  value       = "${module.bastion_host.address}"
+  value       = module.bastion_host.address
 }
 
 output "private_instance" {
   description = "A reference (self_link) to the private instance"
-  value       = "${google_compute_instance.private.self_link}"
+  value       = google_compute_instance.private.self_link
 }
+
