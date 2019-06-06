4a90f39c6bd90b59d865c78068d7e9f1bf53f900
Rob Morgan <robbym@gmail.com>
2019-06-06 15:40:30 +0200

    Revert "ensure the firewall resource is created before its destroyed"
    This reverts commit d42e870aaca9d361ac0ab358788bbdb03c1f648a.

--- modules/network-firewall/main.tf
+++ modules/network-firewall/main.tf
@@ -59,10 +59,6 @@ resource "google_compute_firewall" "private_allow_all_network_inbound" {
   allow {
     protocol = "all"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
