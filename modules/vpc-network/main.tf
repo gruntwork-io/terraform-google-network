e845dcd284fbca41a4ad36f1daee8cd321cf39e4
Olaf Schoenwald <olaf.schoenwald@plusserver.com>
2020-03-21 09:36:17 +0100

    Fixes: Error: "enable_flow_logs": [REMOVED] This field is being removed in favor of log_config.

--- modules/vpc-network/main.tf
+++ modules/vpc-network/main.tf
@@ -54,6 +54,9 @@ resource "google_compute_subnetwork" "vpc_subnetwork_public" {
   }
 
   log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
   }
 }
 
