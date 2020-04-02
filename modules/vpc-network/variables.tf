1ecef5e6f0cc4b7a3a9b683b88120fc22c74bd2f
Rob Morgan <robbym@gmail.com>
2020-04-02 19:33:06 +0200

    [skip ci] improve docs

--- modules/vpc-network/variables.tf
+++ modules/vpc-network/variables.tf
@@ -60,7 +60,7 @@ variable "secondary_cidr_subnetwork_spacing" {
 }
 
 variable "log_config" {
-  description = "The logging options for the subnetwork flow logs. See https://www.terraform.io/docs/providers/google/r/compute_subnetwork.html for more information and examples."
+  description = "The logging options for the subnetwork flow logs. Setting this value to `null` will disable them. See https://www.terraform.io/docs/providers/google/r/compute_subnetwork.html for more information and examples."
   type = object({
     aggregation_interval = string
     flow_sampling        = number
