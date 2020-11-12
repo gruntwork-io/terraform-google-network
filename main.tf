69c6eb1ab1f177d018b7e5f9114dfb8d53fea13c
Rob Morgan <robbym@gmail.com>
2020-11-12 11:46:13 +0800

    update for tf 0.13

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 terraform {
   # The modules used in this example have been updated with 0.12 syntax, which means the example is no longer
   # compatible with any versions below 0.12.
-  required_version = ">= 0.12"
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
