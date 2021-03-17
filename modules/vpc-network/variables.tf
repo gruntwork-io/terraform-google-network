dc0cd364889d03acd2078573209281ca97f2f415
Elias <eliasscosta@gmail.com>
2021-03-17 15:17:14 -0300

    add variable for second_secondary_range

--- modules/vpc-network/variables.tf
+++ modules/vpc-network/variables.tf
@@ -47,6 +47,12 @@ variable "secondary_cidr_block" {
   default     = "10.1.0.0/16"
 }
 
+variable "second_secondary_cidr_block" {
+  description = "The IP address range of the VPC's secondary address range in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
 variable "secondary_cidr_subnetwork_width_delta" {
   description = "The difference between your network and subnetwork's secondary range netmask; an /16 network and a /20 subnetwork would be 4."
   type        = number
